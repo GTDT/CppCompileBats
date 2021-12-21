b24846e01dc40a2a490b907f6501c10c7ccd6ef8
GT <66834354+GTDT@users.noreply.github.com>
2021-12-21 19:32:16 +0200

    Update DCompile.bat

--- Scripts/DCompile.bat
+++ Scripts/DCompile.bat
@@ -1,7 +1,4 @@
 @ECHO OFF
 
-rem with -Os (optimisation for size) tag.
+:: with -Os (optimisation for size) tag.
 gcc .\main.cpp -ggdb -o main.exe
-
-rem with -1 (maximum compression) tag.
-rem upx .\main.exe -9
