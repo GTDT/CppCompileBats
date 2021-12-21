38e09b003addcb961e9643aacccf06f5f4b678fa
GT <66834354+GTDT@users.noreply.github.com>
2021-12-21 19:33:02 +0200

    Update RSCompile.bat

--- Scripts/RSCompile.bat
+++ Scripts/RSCompile.bat
@@ -1,10 +1,7 @@
 @ECHO OFF
 
-rem Clearing the screen.
-rem cls
-
-rem with -Os (optimisation for size) tag.
+:: with -Os (optimisation for size) tag.
 gcc .\main.cpp -Os -o main.exe
 
-rem with -1 (maximum compression) tag.
+:: with -1 (maximum compression) tag.
 upx .\main.exe -9
